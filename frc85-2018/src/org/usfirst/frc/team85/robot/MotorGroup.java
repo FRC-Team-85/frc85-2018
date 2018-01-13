a6e33160a994608b452e370952cb17d40486eb8a
foxmason <foxmasonm@gmail.com>
2018-01-13 10:35:38 -0500

    MotorGroup class

---
+++ frc85-2018/src/org/usfirst/frc/team85/robot/MotorGroup.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team85.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class MotorGroup {
+
+	TalonSRX[] motors;
+	double[] bias;
+
+	public MotorGroup(int[] a) {
+		motors = new TalonSRX[a.length];
+
+		for (int i = 0; i < a.length; i++) {
+			motors[i] = new TalonSRX(a[i]);
+		}
+	}
+
+	public void setPower(double power) {
+		for (int i = 0; i < motors.length; i++) {
+			motors[i].set(ControlMode.PercentOutput, power);
+		}
+	}
+	
+	public double[] getCurrent() {
+		double[] current = new double[motors.length];
+		for (int i = 0; i < motors.length; i++) {
+			current[i] = motors[i].getOutputCurrent();
+		}
+		
+		return current;
+	}
+}
