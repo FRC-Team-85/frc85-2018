7e6427a8b8cb7eafea5ef3efaa300ef55cb666bd
Craig Smith <craigjsmith99@gmail.com>
2018-01-20 08:19:07 -0500

    Revert "drive class, no follow mode"
    This reverts commit e8de1cca0189f3acf1fccfa7cb2896f867c7d9b8.

--- frc85-2018/src/org/usfirst/frc/team85/robot/Addresses.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team85.robot;
-
-public class Addresses {
-
-	public final static int leftFrontTalon = 1;
-	public final static int leftBackTalon = 2;
-	public final static int rightFrontTalon = 3;
-	public final static int rightBackTalon = 4;
-}
