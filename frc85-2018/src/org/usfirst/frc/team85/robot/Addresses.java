89eceaa165e43568db767c5a323e0cc8b6067bae
Craig Smith <craigjsmith99@gmail.com>
2018-01-20 08:26:33 -0500

    addresses

---
+++ frc85-2018/src/org/usfirst/frc/team85/robot/Addresses.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team85.robot;
+
+public class Addresses {
+
+	public final static int leftFrontTalon = 1;
+	public final static int leftBackTalon = 2;
+	public final static int rightFrontTalon = 4;
+	public final static int rightBackTalon = 3;
+}
