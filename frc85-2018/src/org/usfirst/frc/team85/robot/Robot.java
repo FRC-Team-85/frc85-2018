7d43ce7f3a9bb5de472a078b878056d935cdce18
foxmason <foxmasonm@gmail.com>
2018-01-30 19:47:01 -0500

    Robot FPS turning and amplitude incorporation

--- frc85-2018/src/org/usfirst/frc/team85/robot/Robot.java
+++ frc85-2018/src/org/usfirst/frc/team85/robot/Robot.java
@@ -45,40 +45,42 @@ public class Robot extends SuperStructure {
 		double speedRight = 0;
 		double speedLeft = 0;
 		double power = (double) SmartDashboard.getNumber("Power", 1);
+		double amplitude = (double) SmartDashboard.getNumber("Amplitude", .5);
+
+		if (Math.abs(_rightJoystick.getRawAxis(1)) >= .1) {
+			speedRight = Math.pow(_rightJoystick.getRawAxis(1), power);
+		} else if (Math.abs(_rightJoystick.getRawAxis(1)) < .1) {
+			speedRight = 0;
+		}
+
+		if (Math.abs(_leftJoystick.getRawAxis(1)) >= .1) {
+			speedLeft = Math.pow(_leftJoystick.getRawAxis(1), power);
+		} else if (Math.abs(_leftJoystick.getRawAxis(1)) < .1) {
+			speedLeft = 0;
+		}
 
 		/*
-		 * if (Math.abs(_rightJoystick.getRawAxis(1)) >= .1) { speedRight =
-		 * Math.pow(_rightJoystick.getRawAxis(1), power); } else if
-		 * (Math.abs(_rightJoystick.getRawAxis(1)) < .1) { speedRight = 0; }
+		 * if (Math.abs(_leftJoystick.getRawAxis(1)) <= .1) { speedLeft = 0; } else if
+		 * (Math.abs(_leftJoystick.getRawAxis(1)) < .9) { speedLeft = 2.5 *
+		 * _leftJoystick.getRawAxis(1) * _leftJoystick.getRawAxis(1) - 1.25 *
+		 * _leftJoystick.getRawAxis(1) + .1; } else { speedLeft = 1; }
 		 * 
-		 * if (Math.abs(_leftJoystick.getRawAxis(1)) >= .1) { speedLeft =
-		 * Math.pow(_leftJoystick.getRawAxis(1), power); } else if
-		 * (Math.abs(_leftJoystick.getRawAxis(1)) < .1) { speedLeft = 0; }
+		 * if (Math.abs(_rightJoystick.getRawAxis(1)) <= .1) { speedRight = 0; } else if
+		 * (Math.abs(_rightJoystick.getRawAxis(1)) < .9) { speedRight = 2.5 *
+		 * _rightJoystick.getRawAxis(1) * _rightJoystick.getRawAxis(1) - 1.25 *
+		 * _rightJoystick.getRawAxis(1) + .1; } else { speedRight = 1; }
 		 */
 
-		if (Math.abs(_leftJoystick.getRawAxis(1)) <= .1) {
-			speedLeft = 0;
-		} else if (Math.abs(_leftJoystick.getRawAxis(1)) < .9) {
-			speedLeft = 2.5 * _leftJoystick.getRawAxis(1) * _leftJoystick.getRawAxis(1)
-					- 1.25 * _leftJoystick.getRawAxis(1) + .1;
-		} else {
-			speedLeft = 1;
-		}
-
-		if (Math.abs(_rightJoystick.getRawAxis(1)) <= .1) {
-			speedRight = 0;
-		} else if (Math.abs(_rightJoystick.getRawAxis(1)) < .9) {
-			speedRight = 2.5 * _rightJoystick.getRawAxis(1) * _rightJoystick.getRawAxis(1)
-					- 1.25 * _rightJoystick.getRawAxis(1) + .1;
-		} else {
-			speedRight = 1;
-		}
-
 		if (_rightJoystick.getRawButton(7)) {
-			power = .5;
+			power = 1;
 		}
 		if (_rightJoystick.getRawButton(8)) {
 			power = 3;
+		}
+		if (_leftJoystick.getRawButton(1)) {
+			amplitude = .75;
+		} else {
+			amplitude = .5;
 		}
 
 		if (_rightJoystick.getRawButton(1) && Math.abs(_rightJoystick.getRawAxis(1)) > .1) {
@@ -86,9 +88,17 @@ public class Robot extends SuperStructure {
 			speedRight = (_rightJoystick.getRawAxis(1));
 
 			if (_leftJoystick.getRawAxis(0) > .1) {
-				speedLeft = _rightJoystick.getRawAxis(1) - _leftJoystick.getRawAxis(0) * .5;
+				if (_rightJoystick.getRawAxis(1) > 0) {
+					speedRight = _rightJoystick.getRawAxis(1) - _leftJoystick.getRawAxis(0) * amplitude;
+				} else {
+					speedRight = _rightJoystick.getRawAxis(1) + _leftJoystick.getRawAxis(0) * amplitude;
+				}
 			} else if (_leftJoystick.getRawAxis(0) < -.1) {
-				speedRight = _rightJoystick.getRawAxis(1) - _leftJoystick.getRawAxis(0) * .5;
+				if (_rightJoystick.getRawAxis(1) > 0) {
+					speedLeft = _rightJoystick.getRawAxis(1) + _leftJoystick.getRawAxis(0) * amplitude;
+				} else {
+					speedLeft = _rightJoystick.getRawAxis(1) - _leftJoystick.getRawAxis(0) * amplitude;
+				}
 			}
 		}
 
@@ -96,6 +106,7 @@ public class Robot extends SuperStructure {
 		mgLeft.setPower(-speedLeft);
 
 		SmartDashboard.putNumber("Power", power);
+		SmartDashboard.putNumber("Amplitude", amplitude);
 	}
 
 	@Override
