64ab7c3b4c9df40583a118c1c58b26d3b8df1d53
tylergutknecht <34144593+tylergutknecht@users.noreply.github.com>
2018-02-12 16:16:37 -0600

    #justintakethings
    better intake logic

--- frc85-2018/src/org/usfirst/frc/team85/robot/Intake.java
+++ frc85-2018/src/org/usfirst/frc/team85/robot/Intake.java
@@ -2,13 +2,14 @@ package org.usfirst.frc.team85.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+
 public class Intake {
 
 	Globals globals = Globals.getInstance();
 
 	private boolean isIntakeOpen;
-	private double wheelSpeed;	// optimal wheel speed determined by tests. later theoretical support for
-								// smartdashboard
+	DigitalInput _leftCubeLimit, _rightCubeLimit;
 
 	public Intake() {
 
@@ -24,11 +25,19 @@ public class Intake {
 		return isIntakeOpen;
 	}
 
-	public void setIntakeWheels(boolean on) {
-		wheelSpeed = 1; // only necessary right now
+	public void setIntakeWheels(boolean on, int wheelSpeed) {
 		if (on) {
-			globals.getLeftIntakeWheel().set(ControlMode.PercentOutput, wheelSpeed);
-			globals.getRightIntakeWheel().set(ControlMode.PercentOutput, wheelSpeed);
+			if (!_leftCubeLimit.get()) {
+				globals.getLeftIntakeWheel().set(ControlMode.PercentOutput, wheelSpeed);
+			} else {
+				globals.getLeftIntakeWheel().set(ControlMode.PercentOutput, 0);
+			}
+			
+			if (!_rightCubeLimit.get()) {
+				globals.getRightIntakeWheel().set(ControlMode.PercentOutput, wheelSpeed);
+			} else {
+				globals.getRightIntakeWheel().set(ControlMode.PercentOutput, 0);
+			}
 		} else {
 			globals.getLeftIntakeWheel().set(ControlMode.PercentOutput, 0);
 			globals.getRightIntakeWheel().set(ControlMode.PercentOutput, 0);
