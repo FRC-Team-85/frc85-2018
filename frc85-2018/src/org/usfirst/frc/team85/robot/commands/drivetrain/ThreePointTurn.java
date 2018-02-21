aa68ccb2e0e338113d7c1dfc4690b08d19cb0e3f
Craig Smith <craigjsmith99@gmail.com>
2018-02-20 20:36:08 -0500

    three point turn?

---
+++ frc85-2018/src/org/usfirst/frc/team85/robot/commands/drivetrain/ThreePointTurn.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team85.robot.commands.drivetrain;
+
+import org.usfirst.frc.team85.robot.subsystems.DriveTrain;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class ThreePointTurn extends CommandGroup {
+	
+	double speed;
+	double strafeDistance;
+	double backDistance;
+	double forwardDistance;
+	double turnDegrees;
+	boolean finished = false;
+
+	public ThreePointTurn(double speed, double strafeDistance, double backDistance) {
+		requires(DriveTrain.getInstance());
+		
+		this.strafeDistance = strafeDistance;
+		this.speed = speed;
+		this.backDistance = backDistance;
+		turnDegrees = Math.asin(strafeDistance/backDistance);
+		forwardDistance = Math.tan(turnDegrees)/strafeDistance;
+		
+		addSequential(new DriveStraight(-speed, backDistance));
+		addSequential(new SpinExactDegrees(turnDegrees));
+		addSequential(new DriveStraight(-speed, forwardDistance));
+	}
+}
